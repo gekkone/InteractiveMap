2670d11ea6f84af87fe6913c73c7932cbdfe76cc
Gekkone <sergej.soloyov@yandex.ru>
2020-04-10 22:11:33 +0300

    Добавлен игрок Изображения теперь передаются в конструктор карты и игрока, оъекты инициализируются только после загрузки ресурсов сцены

--- app/resources/js/game/objects/gameMap.js
+++ app/resources/js/game/objects/gameMap.js
@@ -2,17 +2,58 @@ import ImageLoader from "../imageLoader"
 import DisplayObject from '../displayObject'
 
 export default class GameMap extends DisplayObject {
-    constructor(scene) {
+    constructor(image, scene) {
         super(scene);
-        this.width = scene.game.canvas.width;
-        this.height = scene.game.canvas.height;
+        this._image = image;
+        // let xScale =  this.scene.camera.width / image.width;
+        // let yScale =  this.scene.camera.height / image.height;
+        this._scale = 0.7;
+    }
+
+    // У камеры иной расчёт координат потому что они определяют
+    // левый верхний угол изображения с которого отрисовывается карта
+    get x() {
+        return this.scene.camera != null ? this._x + this.scene.camera.x : this._x;
+    }
+    get y() {
+        return this.scene.camera != null ? this._y + this.scene.camera.y : this._y;
     }
 
     render(time) {
-        this.scene.game.context.drawImage(this.scene.imageResources.get('map'),
-            this.x, this.y, this.scene.camera.width, this.scene.camera.height,
-            0, 0, this.scene.camera.width, this.scene.camera.height
-        );
+        let width = this.scene.camera.width / this.scene.camera.scale / this._scale;
+        let height = this.scene.camera.height / this.scene.camera.scale / this._scale;
+        this.scene.game.context.drawImage(
+            this._image,
+            this.x, this.y, width, height,
+            0, 0, this.scene.camera.width, this.scene.camera.height);
+
+        let pos = this.posFrom(1655, 925);
+        this.scene.game.context.fillStyle = "orange";
+        this.scene.game.context.fillRect(pos.x - 5, pos.y - 5, 10 , 10);
         super.render(time);
+    }
+
+    posTo(canvasX, canvasY) {
+        if (this.scene.camera != null) {
+            canvasX += this.scene.camera.x;
+            canvasY += this.scene.camera.y;
+        }
+
+        canvasX /= this._scale;
+        canvasY /= this._scale;
+
+        return { x: canvasX, y: canvasY };
+    }
+
+    posFrom(mapX, mapY) {
+        mapX *= this._scale;
+        mapY *= this._scale;
+
+        if (this.scene.camera != null) {
+            mapX -= this.scene.camera.x;
+            mapX -= this.scene.camera.y
+        }
+
+        return { x: mapX, y: mapY };
     }
 }
--- app/resources/js/game/scenes/questScene.js
+++ app/resources/js/game/scenes/questScene.js
@@ -2,27 +2,38 @@ import Scene from '../scene'
 import ImageLoader from '../imageLoader';
 import GameMap from '../objects/gameMap';
 import Camera from '../camera';
+import Player from '../objects/player';
 
 export default class QuestScene extends Scene {
     constructor(name, game) {
         super(name, game)
-
-        let map = new GameMap(this);
-        this.addObject('map', map);
-
-        this.camera = new Camera(-200, -500, this.game.canvas.width, this.game.canvas.height, this);
+        this.camera = new Camera(0, 0, this.game.canvas.width, this.game.canvas.height, this);
     }
 
     load() {
-        let loader = new ImageLoader(new Map([['map', '/img/game/map.jpg']]));
+        let loader = new ImageLoader(new Map([
+            ['map', '/img/game/map.jpg'],
+            ['player', '/img/game/player.png']
+        ]));
 
         return new Promise((resolve, reject) => {
             loader.load().then(() => {
                 this.imageResources = loader.images;
+
+                this._initObjects();
+
                 this._isLoad = true;
             }).catch((e) => {
                 reject(e);
             });
         });
+    }
+
+    _initObjects() {
+        let map = new GameMap(this.imageResources.get('map'), this);
+        this.addObject('map', map);
+
+        let player = new Player(this.imageResources.get('player'), this);
+        this.addObject('player', player);
     }
 }
