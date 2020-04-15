2cd2d0b25a179c2a4fabce725d9a8279c4b84cd5
Gekkone <sergej.soloyov@yandex.ru>
2020-04-15 04:07:46 +0300

    Для камеры реализован режим следования за объектом

--- app/resources/js/game/camera.js
+++ app/resources/js/game/camera.js
@@ -1,36 +1,61 @@
 export default class Camera {
     constructor(x, y, width, height, scene) {
-        this._x = x;
-        this._y = y;
+        this.x = x;
+        this.y = y;
 
-        this._width = width;
-        this._heigth = height;
+        this.width = width;
+        this.height = height;
         this._scene = scene;
         this._scale = 1;
 
         this._scene.game.addEventListener('resizeCanvas', (event) => {
-            this._width = event.detail.width;
-            this._heigth = event.detail.height;
+            this.width = event.detail.width;
+            this.height = event.detail.height;
         });
-    }
 
-    get x() {
-        return this._x;
-    }
-    get y() {
-        return this._y;
-    }
-    get width() {
-        return this._width;
-    }
-    get height() {
-        return this._heigth;
+        this._followAt = null;
+        this._followDistance = 0;
     }
 
     get scale() {
         return this._scale;
     }
-    set scale(value) {
-        this._scale = value;
+    set scale(scale) {
+        let x = this.x / this._scale;
+        let y = this.y / this._scale;
+
+        this._scale = scale;
+        this.x = x * this._scale;
+        this.y = y * this._scale;
+    }
+
+    followAt(object, distance) {
+        this._followAt = object;
+        this._followDistance = distance;
+    }
+
+    render(time) {
+        if (this._followAt == null) {
+            return;
+        }
+
+        if (this._followAt.x + this._followAt.width > this.x + this.width - this._followDistance) {
+            this.x = Math.min(
+                this._scene.x + this._scene.width,
+                this._followAt.x + this._followAt.width - this.width + this._followDistance
+            );
+        }
+        if (this._followAt.x < this.x + this._followDistance) {
+            this.x = Math.max(this._scene.x, this._followAt.x - this._followDistance);
+        }
+        if (this._followAt.y + this._followAt.height > this.y + this.height - this._followDistance) {
+            this.y = Math.min(
+                this._scene.y + this._scene.height,
+                this._followAt.y + this._followAt.height - this.height + this._followDistance
+            );
+        }
+        if (this._followAt.y < this.y + this._followDistance) {
+            this.y = Math.max(this._scene.y, this._followAt.y - this._followDistance);
+        }
     }
 }
