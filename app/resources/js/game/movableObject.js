cd0244d5def9ce05fdc44eb2555adbce0c924688
Gekkone <sergej.soloyov@yandex.ru>
2020-04-15 04:08:29 +0300

    Исправление бага позиционирования объекта при перемещении

--- app/resources/js/game/movableObject.js
+++ app/resources/js/game/movableObject.js
@@ -5,22 +5,27 @@ export default class MovableObject extends DisplayObject {
         super(scene);
 
         this._movedPosition = { x: -1, y: -1 }; //Позиция на которую следует переместится объекту
-        this._speed = 1000; //Скорость движения объекта - количество ms за которое объект передвигается на 1px
+        this._speed = 100; //Скорость движения объекта - количество ms за которое объект передвигается на 1px
         this._stepX = 1; //Сколько пикселей должен занимать один шаг движения по X
         this._stepY = 1; //Сколько пикселей должен занимать один шаг движения по Y
         this._lastRender = 0; //Время последней отрисовки
     }
 
+    get movedPosition() {
+        return this._movedPosition;
+    }
+
     // Перемещает объект в указанную точку
-    moveTo(x, y, speed = 0) {
-        this._movedPosition.x = x;
-        this._movedPosition.y = y;
+    moveTo(sceneX, sceneY, speed = 0) {
+        this._movedPosition.x = sceneX;
+        this._movedPosition.y = sceneY;
+
         if (speed != 0) {
             this._speed = speed;
         }
 
-        let distanceX = Math.abs(this._movedPosition.x - this.x);
-        let distanceY = Math.abs(this._movedPosition.y - this.y);
+        let distanceX = Math.abs(this._movedPosition.x - this._x);
+        let distanceY = Math.abs(this._movedPosition.y - this._y);
 
         if (distanceX < distanceY) {
             this._stepX = distanceX / distanceY;
@@ -48,27 +53,31 @@ export default class MovableObject extends DisplayObject {
 
     // Функция отрисовки объекта
     render(time) {
+        //TODO Проверка на выход за границы сцены
         if (this.isMoves()
             && (this._lastRender == 0 || time - this._lastRender >= this._speed)) {
             let skippedSycleFactor = this._lastRender > 0 ? (time - this._lastRender) / this._speed : 1;
             this._lastRender = time;
 
 
-            this.x = this.x + this._stepX * skippedSycleFactor;
-            this.y = this.y + this._stepY * skippedSycleFactor;
+            this._x = this._x + this._stepX * skippedSycleFactor;
+            this._y = this._y + this._stepY * skippedSycleFactor;
 
 
-            if ((this._stepX > 0 && this.x > this._movedPosition.x)
-                || (this._stepX < 0 && this.x < this._movedPosition.x)) {
-                this.x = this._movedPosition.x;
+            if ((this._stepX > 0 && this._x > this._movedPosition.x)
+                || (this._stepX < 0 && this._x < this._movedPosition.x)) {
+                this._x = this._movedPosition.x;
             }
 
-            if ((this._stepY > 0 && this.y > this._movedPosition.y)
-                || (this._stepY < 0 && this.y < this._movedPosition.y)) {
-                this.y = this._movedPosition.y;
+            if ((this._stepY > 0 && this._y > this._movedPosition.y)
+                || (this._stepY < 0 && this._y < this._movedPosition.y)) {
+                this._y = this._movedPosition.y;
             }
 
-            console.debug(this.x, this.y);
+            if (this._x == this._movedPosition.x && this._y == this._movedPosition.y) {
+                this._lastRender = 0;
+                this.dispatchEvent(new CustomEvent('arrivedPosition'));
+            }
         }
 
         super.render(time);
