abf203df0c4ab60f2d4f6f7e5b9dbd64d150788e
Gekkone <sergej.soloyov@yandex.ru>
2021-01-05 16:11:31 +0300

    Оптимизировал отрисовку карты (на коленке)

--- app/resources/js/game/objects/gameMap.js
+++ app/resources/js/game/objects/gameMap.js
@@ -6,16 +6,63 @@ export default class GameMap extends DisplayObject {
         this._image = image;
         this._width = image.width;
         this._height = image.height;
+
+        this._virtCanvas = document.createElement('canvas');
+        this._cacheContext = this._virtCanvas.getContext('2d');
+
+        this._cacheOption = {
+            x: 0,
+            y: 0,
+            sceneWidth: 0,
+            sceneHeight: 0
+        };
     }
 
     render(time) {
-        let pos = this.canvasPos();
-        // Отрисовывает только видимую область карты.
-        this.scene.game.context.drawImage(
-            this._image,
-            -pos.x, -pos.y, this.width, this.height,
-            0, 0, this.scene.width, this.scene.height);
+        this._preRender();
+
+        this.scene.game.context.drawImage(this._virtCanvas, 0, 0);
 
         super.render(time);
+    }
+
+    _preRender() {
+        let pos = this.canvasPos();
+
+        if (
+            this._virtCanvas.width !== this.scene.game.canvas.width
+            || this._virtCanvas.height !== this.scene.game.canvas.height
+        ) {
+            this._virtCanvas.width = this.scene.game.canvas.width;
+            this._virtCanvas.height = this.scene.game.canvas.height;
+        }
+
+        let option = {
+            x: pos.x,
+            y: pos.y,
+            sceneWidth: this.scene.width,
+            sceneHeight: this.scene.height
+        };
+
+        if (
+            this._cacheOption.x == option.x
+            && this._cacheOption.y == option.y
+            && this._cacheOption.sceneWidth == option.sceneWidth
+            && this._cacheOption.sceneHeight == option.sceneHeight
+        ) {
+            return;
+        }
+
+
+        this._cacheOption = option
+
+        let width = this.scene.camera != null ? this.scene.game.canvas.width * this.scene.camera.scale : this.scene.game.canvas.width;
+        let height = this.scene.camera != null ? this.scene.game.canvas.height * this.scene.camera.scale : this.scene.game.canvas.height;
+
+        this._cacheContext.drawImage(
+            this._image,
+            -option.x, -option.y, width, height,
+            0, 0, this.scene.game.canvas.width, this.scene.game.canvas.height
+        );
     }
 }
